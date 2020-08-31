ff1feb79727cec8a202121c513bedc753677e5e8
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-08-31 15:30:04 -0400

    Add solicitation message to the end of each story

--- src/Story.js
+++ src/Story.js
@@ -23,6 +23,13 @@ class Story extends Component {
           <footer>- <cite> Stranger no. {this.props.stranger_id}</cite></footer>
         </blockquote>
         <div className="narrative" dangerouslySetInnerHTML={this.narrativeHtml()} />
+        <div className="solicitation">
+          helloStranger stories take encounters between strangers as a starting point. 
+          If you like this project please take a moment to&nbsp;
+          <a href="https://airtable.com/shrhBkljBMeLUa4wR" target="_blank" rel="noopener noreferrer">
+            remember a stranger
+          </a>.
+        </div>
       </div>
     );
   }
