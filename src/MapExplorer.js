e8965209c9829449b3e88c959d34f2e258f527fc
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-09-07 14:51:45 -0400

    mapExplorer should not be draggable

--- src/MapExplorer.js
+++ src/MapExplorer.js
@@ -6,7 +6,11 @@ class MapExplorer extends Component {
 
   render() {
     return (
-      <Map className="explorer" center={[43.700056, -79.426948]} zoom={10} zoomControl={false} attributionControl={false}>
+      <Map className="explorer" 
+        center={[43.700056, -79.426948]} // Toronto's centre roughly speaking
+        zoom={10} zoomControl={false} 
+        attributionControl={false}
+        dragging={false}>
         <TileLayer
           url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png' // zoomMax = 20
           // attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
