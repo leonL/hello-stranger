4a45f4855d1498eb7f6c14b9411eede8083ad39d
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-09-10 14:52:32 -0400

    no dragging story map

--- src/StoryMap.js
+++ src/StoryMap.js
@@ -18,7 +18,8 @@ class MapExplorer extends Component {
         center={[43.700056, -79.446948]} // Toronto's centre roughly speaking
         zoom={10} 
         zoomControl={false} 
-        attributionControl={false} >
+        attributionControl={false}
+        dragging={false}> >
         <TileLayer
           url='https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png' // zoomMax = 20
           // attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
