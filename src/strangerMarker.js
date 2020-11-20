e159c21245531d6591f80db71d6aa71574abfe4b
Leon Lukashevsky <leon.lukashevsky@gmail.com>
2020-11-20 01:00:30 -0500

    bigger

--- src/strangerMarker.js
+++ src/strangerMarker.js
@@ -3,11 +3,11 @@ import L from 'leaflet';
 const strangerMarker = new L.Icon({
     iconUrl: require('./mapIcon.png'),
     // shadowUrl: null,
-    iconSize: [30, 30],
+    iconSize: [40, 40],
     // shadowSize: null,
-    iconAnchor: [15, 30],
+    iconAnchor: [20, 40],
     // shadowAnchor: null,
-    popupAnchor: [0, -35],
+    popupAnchor: [0, -45],
     className: 'stranger-marker'
 });
 
